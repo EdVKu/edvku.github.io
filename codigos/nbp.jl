f6c90dff9a8971c62d416f02ccc5c8de7b74b7a3
EdVKu <eduardovkuri@ciencias.unam.mx>
2025-02-28 02:14:37 +0000

    Deploying to gh-pages from  @ 5a3a42d1494a7ba480cef8858a5b5e568d3ae8a7 🚀

--- codigos/nbp.jl
+++ codigos/nbp.jl
@@ -1,16 +1,20 @@
+# Extraemos paquetes de visualización
+
+
+
 # masas de los cuerpos 
 # (en kg)
 
-M_1 = 1e5
-M_2 = 1e10
+M_1 = 2e30
+M_2 = 5.97e24
 
 # velocidad angular inicial, distancia radial, y cambio de distancia radial 
 # respecto al tiempo
 # (en radianes, m, m/s)
 
-w, rho, rdo = 1e-3, 1e7, 0
+w, rho, rdo = (2*pi)/(365.25*24*3600), 1.5e11, 0
 
-t, h, N = 0, 1e-3, 1e5
+t, h, N = 0, 1e0, 365.25*24*3600
 
 # constante gravitacional
 # (en m^3 kg^-1 s^-2)
@@ -39,26 +43,26 @@ function thdd(r, rd)
 end
 
 
-#so = [t, θ, ρ, ̇ρ]
+#so = [t, θ, ρ]
 #son las condiciones iniciales para el sistema de dos cuerpos
-so = [t, 0, rho, rdo]
-xyto = [xy(rho,0); t]
-s3 = xyto
 
-
+x0 = rho + rdo*h + 1/2*rdd(rho)*h^2
+θ0 = h*thd(rho)
+so = [[t, 0, rho],[t+h, θ0, x0]]
+# xyto = [mu/M_1*xy(rho,0); t]
+s3 = so
+t+=2*h
 while t<h*(N)
-    s = so
-    dρ_12 = s[4] + h/2*rdd(s[3])
-    ρ_1 = s[3] + h*dρ_12
-    dρ_1 = dρ_12 + h/2*ρ_1
-    θ_1 = s[2] + h*thd(ρ_1)
+    s = so 
+    ls = last(s,2)
+    ρ_1 = (2*ls[2][3] - ls[1][3] + rdd(ls[2][3])*h^2/2)
+    θ_1 = ls[1][2] + h*thd(ρ_1)
     global t += h
-    s2 = [t, θ_1, ρ_1, dρ_1]
+    s2 = [ls[1],[t, θ_1, ρ_1]]
 
     global so = s2
-    xyt = [xy(ρ_1,θ_1); t]
-    global s3 = [[s3]; [xyt]]
+    # xyt = [mu/M_1*xy(ρ_1,θ_1); t]
+    push!(s3, last(s2))
 end
 
-
-
+print(last(s3,4))
